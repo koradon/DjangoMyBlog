9e751c70bc943d1de5dcd4d00184d494c680ec85
Michał Sipa <michal.sipa@gmail.com>
2016-11-03 15:07:36 +0100

    updates to admins posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from posts.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "updated", "timestamp"]
+    filter = ["updated"]
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostAdmin)
